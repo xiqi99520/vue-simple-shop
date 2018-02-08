5e4e2c43920b2ab76840533fac3bb9d3b65132c7
feng <352903215@qq.com>
2018-02-09 00:05:26 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <keep-alive>
+        <router-view></router-view>
+     </keep-alive>
+    </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="banner">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      msg: 'Welcome',
+      tip: 'UP TO 70% OFF'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.banner {
+  width: 100%;
+  height: 198px;
+  background: url(../assets/1.png) no-repeat center center;
+  background-size: 100% auto;
+}
+</style>
