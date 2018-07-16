82f7ac21e95a442a3bf7aebff3d2689335f66f32
Feng <352903215@qq.com>
2018-07-16 17:31:58 +0800

    修改config里的build配置

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-simple-shop/',
+    assetsPublicPath: '/vue-simple-shop/dist/',
 
     /**
      * Source Maps
